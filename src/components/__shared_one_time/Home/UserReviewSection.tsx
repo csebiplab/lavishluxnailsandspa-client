042808ca66dd811b667e08517aedb60e7f604887
Oliulla <razasm29@gmail.com>
2024-02-05 19:50:39 +0600

    :sparkles: Done Review Section

--- src/components/__shared_one_time/Home/UserReviewSection.tsx
+++ src/components/__shared_one_time/Home/UserReviewSection.tsx
@@ -1,13 +1,53 @@
-import React from "react";
-import ReveiwCard from "../__ui/ReviewCard";
+import ReviewCard from "../__ui/ReviewCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation } from "swiper/modules";
 
 const UserReviewSection = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-      {[1, 2, 3, 4, 5, 6].map((__, i) => (
-        <ReveiwCard key={i} />
-      ))}
-    </div>
+    <>
+      <Swiper
+        navigation={true}
+        loop={true}
+        modules={[Navigation]}
+        slidesPerView={4}
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+          },
+          400: {
+            slidesPerView: 2,
+          },
+          639: {
+            slidesPerView: 3,
+          },
+          865: {
+            slidesPerView: 4,
+          },
+          1000: {
+            slidesPerView: 5,
+          },
+          1500: {
+            slidesPerView: 6,
+          },
+          1700: {
+            slidesPerView: 7,
+          },
+        }}
+        spaceBetween={50}
+        className=""
+      >
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((__, i) => (
+          <SwiperSlide key={i} className="w-full md:w-auto">
+            <ReviewCard />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 };
 
--- src/components/__shared_one_time/__ui/ReviewCard.tsx
+++ src/components/__shared_one_time/__ui/ReviewCard.tsx
@@ -1,12 +1,16 @@
-import { Card } from "flowbite-react";
+import { FaStar } from "react-icons/fa";
 
-export default function ReveiwCard() {
+export default function ReviewCard() {
   return (
-    <div className="max-w-sm bg-white shadow-lg px-6 py-4">
-      <h5 className="text-2xl font-bold tracking-tight">
-        Noteworthy technology acquisitions 2021
-      </h5>
-      <p className="font-normal">
+    <div className="max-w-sm bg-white shadow-lg px-6 py-8">
+      <div className="flex gap-x-1">
+        <FaStar className="w-4 h-4 text-yellow-400" />
+        <FaStar className="w-4 h-4 text-yellow-400" />
+        <FaStar className="w-4 h-4 text-yellow-400" />
+        <FaStar className="w-4 h-4 text-yellow-400" />
+        <FaStar className="w-4 h-4 text-yellow-400" />
+      </div>
+      <p className="font-normal mt-4">
         Here are the biggest enterprise technology acquisitions of 2021 so far,
         in reverse chronological order.
       </p>
