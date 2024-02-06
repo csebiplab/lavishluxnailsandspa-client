adbac2ce0ecf0ffb4cc0617f37eef5bf6b824ef5
Oliulla <razasm29@gmail.com>
2024-02-06 15:23:26 +0600

    :hammer: Optimised Accordian

--- src/pages/theme.ts
+++ src/pages/theme.ts
@@ -9,4 +9,27 @@ export const flowbiteTheme: CustomFlowbiteTheme = {
       },
     },
   },
+  accordion: {
+    root: {
+      base: "divide-y divide-gray-200 border-gray-200 dark:divide-gray-700 dark:border-gray-700",
+      flush: {
+        off: "rounded-lg border",
+        on: "border-b",
+      },
+    },
+    content: {
+      base: "py-5 px-5 last:rounded-b-lg primaryBgColor first:rounded-t-lg",
+    },
+    title: {
+      base: "flex w-full items-center justify-between first:rounded-t-lg last:rounded-b-lg py-5 px-5 text-left font-medium text-gray-600 dark:text-gray-400",
+      flush: {
+        off: "hover:bg-yellow-600",
+        on: "bg-transparent",
+      },
+      open: {
+        off: "",
+        on: "text-gray-100 bg-yellow-600",
+      },
+    },
+  },
 };
